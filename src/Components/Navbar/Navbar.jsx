e1e1e7c277668043b68750129c8ea27d43b48f1f
iftakhersifat <hossensifat615@gmail.com>
2025-05-09 23:10:12 +0600

    added updated navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,10 +6,23 @@ const Navbar = () => {
     return (
         <div className='container mx-auto flex justify-between mt-10'>
             <div className='flex gpa-4'>
-                <span className="text-4xl">🌎</span>
-                <h1 className="text-3xl font-bold text-blue-600">Country Explorer</h1>
+            <div className="dropdown">
+           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+        </div>
+        <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow border border-amber-500">
+        <NavLink to="/" className={({isActive})=> isActive ? "text-amber-500 text-lg underline" : "text-lg"}>Home</NavLink>
+        <NavLink to="/about" className={({isActive})=> isActive ? "text-amber-500 text-lg underline" : "text-lg"}>About Us</NavLink>
+        </ul>
+        </div>
+        <span className="text-4xl">🌎</span>
+            <h1 className="text-3xl font-bold text-blue-600">Country Explorer</h1>
             </div>
-            <div className='space-x-4'>
+
+            {/* link section */}
+            <div className='space-x-4 lg:flex hidden'>
             <NavLink to="/" className={({isActive})=> isActive ? "text-amber-500 text-lg underline" : "text-lg"}>Home</NavLink>
             <NavLink to="/about" className={({isActive})=> isActive ? "text-amber-500 text-lg underline" : "text-lg"}>About Us</NavLink>
             </div>
