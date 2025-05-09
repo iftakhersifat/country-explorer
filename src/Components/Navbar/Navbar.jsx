0f10725a0f87956c1fca2a23eb747a502d835a51
iftakhersifat <hossensifat615@gmail.com>
2025-05-09 22:58:48 +0600

    added navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BiWorld } from 'react-icons/bi';
 import { Link, NavLink } from 'react-router';
 
 const Navbar = () => {
     return (
-        <div className='container mx-auto'>
-            <NavLink to="/" className={({isActive})=> isActive ? "text-amber-500 underline" : ""}>Home</NavLink>
-            <NavLink to="/about" className={({isActive})=> isActive ? "text-amber-500 underline" : ""}>About Us</NavLink>
+        <div className='container mx-auto flex justify-between mt-10'>
+            <div className='flex gpa-4'>
+                <span className="text-4xl">🌎</span>
+                <h1 className="text-3xl font-bold text-blue-600">Country Explorer</h1>
+            </div>
+            <div className='space-x-4'>
+            <NavLink to="/" className={({isActive})=> isActive ? "text-amber-500 text-lg underline" : "text-lg"}>Home</NavLink>
+            <NavLink to="/about" className={({isActive})=> isActive ? "text-amber-500 text-lg underline" : "text-lg"}>About Us</NavLink>
+            </div>
         </div>
     );
 };
